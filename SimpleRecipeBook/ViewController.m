8b323d013128a4aa5694fbbf8726caf0fa2d0146
Riley Fewell <rileyfewell@Rileys-MacBook-Pro.local>
2016-02-18 13:59:41 -0800

    initial commit

--- SimpleRecipeBook/ViewController.m
+++ SimpleRecipeBook/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSArray * recipes;
+}
 
 @end
 
@@ -17,6 +20,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    recipes = [NSArray arrayWithObjects: @"Eggs Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle With Egg", @"Noodle With BBQ Pork", @"Japanese Noodle With Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham And Cheese Panini", nil];
+}
+
+// UITABLEVIEW DATASOURCE METHODS
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"RecipeCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
